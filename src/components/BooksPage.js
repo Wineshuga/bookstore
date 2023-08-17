0fc869c08bddaf41337cfe28fbbcb80624a29115
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-17 17:18:30 +0100

    Create BooksPage component

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,8 +1,36 @@
 import AddBook from './AddBook';
+import Book from './Book';
 
 const Books = () => (
   <>
-    <div>Books</div>
+    <section className="book-list-container">
+      <ul>
+        <li>
+          <Book
+            bookTitle="The Hunger Games"
+            author="Suzanne Collins"
+            progress="68"
+            page="Chapter 17"
+          />
+        </li>
+        <li>
+          <Book
+            bookTitle="Dune"
+            author="Frank Herbert"
+            progress="8"
+            page='Chapter 3: "A Lesson Learned"'
+          />
+        </li>
+        <li>
+          <Book
+            bookTitle="Capital in the Twenty-First Century"
+            author="Suzanne Collins"
+            progress="0"
+            page="Introduction"
+          />
+        </li>
+      </ul>
+    </section>
     <AddBook />
   </>
 );
