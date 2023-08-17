31d9fde518eeff39f3bbd61dac471ff6126845a5
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-17 14:35:04 +0100

    Setup header component for nav link

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Bookstore CMS</h1>
+    <ul>
+      <li>
+        <Link to="/">BOOKS</Link>
+      </li>
+      <li>
+        <Link to="/categories">CATEGORIES</Link>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Header;
