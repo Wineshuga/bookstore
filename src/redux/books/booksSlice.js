4eda04764a52f7a4a7949d6b1addede22e3b66cb
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-22 13:17:24 +0100

    Implement required changes on booksSlice reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,10 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  books: [],
+};
+
 const booksSlice = createSlice({
   name: 'book',
-  initialState: [],
+  initialState,
   reducers: {
-    addBook: (state) => [...state, { title: 'New Book' }],
+    addBook: (state, action) => {
+      state.books = [
+        ...state.books,
+        {
+          id: Math.floor(Math.random() * 100),
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
+    },
     removeBook: (state, { payload }) => [...state].filter((item) => item.id !== payload),
   },
 });
