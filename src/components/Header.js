3c419b6df25dfc55d5218443c516c4d735b1a84e
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-25 12:37:02 +0100

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
+import styles from '../styles/header.module.css';
 
 const Header = () => (
-  <header>
+  <header className={styles.header}>
     <h1>Bookstore CMS</h1>
-    <ul>
-      <li>
-        <Link to="/">BOOKS</Link>
-      </li>
-      <li>
-        <Link to="/categories">CATEGORIES</Link>
-      </li>
-    </ul>
-    <div>icon</div>
+    <nav className={styles.header_nav}>
+      <ul>
+        <li>
+          <Link to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories">CATEGORIES</Link>
+        </li>
+      </ul>
+      <div className={styles.header_icon}>
+        <span className="material-icons primary-color">person</span>
+      </div>
+
+    </nav>
   </header>
 );
 
